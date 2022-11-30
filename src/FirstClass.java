4e2359129af37ba7a55913801da172d04720a46c
Diana Cozac <platona.diana@gmail.com>
2022-12-01 00:24:52 +0200

    second commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -43,7 +43,18 @@ public class FirstClass {
             }
 
                String s = (isDomestic) ? "This car is domestic" : " This car is not domestic"  ;
-               System.out.println( s );
+               System.out.println(s);
+
+               //chalange 1
+
+        double firstValue = 20.00d ;
+        double secondValue = 80.00d ;
+        double Total = firstValue + secondValue * 100.00 ;
+        System.out.println ( "My Values total is " + Total );
+        double theRemainder = Total % 40.00 ;
+        System.out.println ( "theremainder " + theRemainder );
+        boolean isNoRemainder = (theRemainder == 0) ? false : true ;
+        System.out.println( "isNoRemainder = " + isNoRemainder);
 
 //        }
 
